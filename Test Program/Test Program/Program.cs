1068f9a4df440aa095da02e4c92cedbca6707f7d
Ramil Ramil <ramil-1988@outlook.com>
2019-12-21 16:59:52 +0200

    first commit from Visual Studio

---
+++ Test Program/Test Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
